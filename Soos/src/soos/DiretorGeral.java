3a498a313df91c0f5ec4a2656f9f8f79add6a050
Victor Emanuel Farias da Costa Borges 115110681 <victorefdcb@lcc2-32.lcc.ufcg.edu.br>
2016-04-28 10:19:36 -0300

    commit do diretor

---
+++ Soos/src/soos/DiretorGeral.java
@@ -0,0 +1,14 @@
+package soos;
+
+public class DiretorGeral {
+	private String nome;
+	private int matricula;
+	private int senha;
+	
+	public DiretorGeral (String nome, int matricula, int senha) {
+		this.nome = nome;
+		this.matricula = matricula;
+		this.senha = senha;
+	}
+
+}
